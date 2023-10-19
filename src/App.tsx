4b8ef16d905cf0436fd762479927b3ac9adadf8b
Alessandro <alessandro.distaso@gmail.com>
2023-10-19 11:20:08 +0200

    gnapclient test2

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,136 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+//const { GnapClient } = require('@interop/gnap-client')
+//import { GnapClient } from '@interop/gnap-client-js'
+import { GnapClient } from './'
+
 
 function App() {
   const [count, setCount] = useState(0)
+
+
+
+  // PAGE 1 - Creating a client instance:
+  
+  const store = {} // TODO: Store for persisting `nonce`s, request handles, etc
+
+  const clientDisplay = {
+    name: 'My RP Application',
+    uri: 'https://app.example.com',
+    logo_uri: 'https://app.example.com/logo.png'
+  }
+
+  const capabilities = ['jwsd']
+
+  const key = {
+    proof: 'jwsd',
+    jwks: { keys: [/* ... */] }
+  }
+
+  // If you pass into the constructor params that would normally go into 
+  // `createRequest`, they're stored as defaults that will be auto-included
+  // in requests.
+  const defaults = {
+    user: { /* .. */ },
+    key,
+    interact
+  }
+
+  const auth = new GnapClient({ store, clientDisplay, capabilities, ...defaults })
+
+
+
+
+  // page 2 - Create and send a request (low-level API):
+
+  const interact = {
+    redirect: true, // default
+    callback: {
+      uri: 'https://app.example.com/callback/1234',
+      nonce: 'LKLTI25DK82FX4T4QFZC'
+    }
+  }
+  const resources = {
+    actions: [/* ... */],
+    locations: [/* ... */],
+    datatype: [/* ... */]
+  }
+  
+  const request = auth.createRequest({ resources, interact, key })
+  
+  // Proposed/experimental
+  const { endpoint } = await auth.discover({ server: 'https://as.example.com' })
+  
+  // Send the request to the transaction endpoint
+  const txResponse = await auth.post({ endpoint, request })
+  
+  const { transaction, accessToken, interactionUrl } = txResponse
+  
+  if (accessToken) { /* success! Although see sessionFromResponse() below */ }
+  
+  // `transaction` holds the various handles and nonces, can be used for
+  // continuation requests 
+  
+  if (interactionUrl) {
+    /* send the user's browser to it */
+    transaction.interactRedirectWindow({ interactionUrl }) // or,
+  
+    /* open a popup window and redirect it  */
+    transaction.interactPopup({ interactionUrl }) // or,
+  
+    /* in Node / Express */
+    res.redirect(interactionUrl) // assuming a `res` ServerResponse object
+    // transaction is saved, will be accessible at the `callback.uri`
+  }
+
+
+  // page 3
+  // Parsing the interact_handle from front-end response, at the callback url. This is low-level (to illustrate the protocol),
+  // app devs are expected to use a helper method like sessionFromResponse() instead.
+
+  // get `callbackUrl` from current url (browser) or request url (server)
+  // `transaction` is from the original transaction response
+
+  // this also validates the incoming `hash` value against saved nonces
+  const interactHandle = await transaction.parseFromUrl(callbackUrl)
+
+  const { accessToken } = await transaction.continueWith({ interactHandle })
+
+
+  
+  
+  // page 4 - High-level helper API (used at the callback url, instead of parseFromUrl / continueWith):
+
+  const session = await auth.sessionFromResponse()
+
+  session.accessToken // validated `access_token`
+  session.fetch // wrapped whatwg `fetch()` that makes authenticated requests 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   return (
     <>
---
+++ src/gnapclient.js
@@ -0,0 +1,171 @@
+'use strict'
+
+import axios from 'axios' // todo: consider 'apisauce' instead
+import httpsig from 'http-signature-header'
+
+const DEFAULT_CLIENT = {
+  name: 'My RP Application',
+  uri: 'https://app.example.com',
+  logo_uri: 'https://app.example.com/logo.png'
+}
+
+const DEFAULT_CAPABILITIES = ['httpsig', 'dpop']
+
+export class GnapClient {
+  /**
+   * @param store {object} Store for handles, nonces
+   * @param [clientDisplay] {object}
+   * @param [capabilities] {Array<string>}
+   * @param [user] {object}
+   * @param [key]
+   * @param [interact]
+   * @param [httpsAgent] {https.Agent} Optional override
+   */
+  constructor ({ store = {}, clientDisplay, capabilities, user, key, interact, httpsAgent } = {}) {
+    this.store = store
+    this.clientDisplay = clientDisplay || DEFAULT_CLIENT
+    this.capabilities = capabilities || DEFAULT_CAPABILITIES
+    this.user = user
+    this.key = key
+    this.interact = interact || { redirect: true }
+    this.httpsAgent = httpsAgent
+  }
+
+  /**
+   * @param resources {Array<string>|object}
+   * @param interact
+   * @param key
+   * @param user
+   *
+   * @returns {TxRequest}
+   */
+  createRequest ({ resources, interact = this.interact, key = this.key, user = this.user }) {
+    if (interact.redirect && !interact.callback) {
+      throw new Error('`callback` param is required for redirect interactions.')
+    }
+
+    if (interact.redirect && !interact.callback.nonce) {
+      interact.callback.nonce = this.generateNonce()
+    }
+
+    return new TxRequest({
+      clientDisplay: this.clientDisplay,
+      capabilities: this.capabilities,
+      resources,
+      interact,
+      key,
+      user
+    })
+  }
+
+  generateNonce () {
+    return 'LKLTI25DK82FX4T4QFZC'
+  }
+
+  /**
+   * Returns transaction endpoint URL.
+   * @param server {string}
+   * @returns {Promise<string>}
+   */
+  async discover ({ server }) {
+    // TODO: actually implement
+    // const discoverUrl = new URL('/.well-known/xyz', server)
+    // const { data: { transaction_endpoint: endpoint } } = await get(discoverUrl.toString())
+    // return endpoint
+    return server + '/transaction'
+  }
+
+  /**
+   * @param server
+   * @param endpoint
+   * @param request
+   * @returns {Promise<{accessToken: string, transaction: {}, interactionUrl: string}>}
+   */
+  async post ({ server, endpoint, request }) {
+    if (!server && !endpoint) {
+      throw new Error('Either endpoint or server param is required.')
+    }
+
+    if (!endpoint) {
+      endpoint = await this.discover({ server }) // throws error if not discoverable
+    }
+
+    const headers = this.headersFor({ request })
+    const { httpsAgent } = this
+
+    const { data: response } = await axios.post(endpoint, request,
+      { headers, httpsAgent })
+
+    const {
+      access_token: accessToken,
+      interaction_url: interactionUrl
+    } = response
+
+    return {
+      transaction: Transaction.from({ response, clientNonce: request.clientNonce }),
+      accessToken,
+      interactionUrl
+    }
+  }
+
+  headersFor ({ request }) {
+    // const { createSignatureString, createAuthzHeader } = httpsig
+    return {
+      // content-type automatically set by axios
+      // 'Detached-JWS': '...'
+      // http signature headers go here
+    }
+  }
+}
+
+export class Transaction {
+  constructor ({ handles, clientNonce, serverNonce } = {}) {
+    this.handles = handles || {
+      // handle (tx handle)
+      // display_handle
+      // interact_handle
+      // user_handle
+      // resource_handle
+      // key_handle
+    }
+    this.clientNonce = clientNonce
+    this.serverNonce = serverNonce
+  }
+
+  static from ({ response, clientNonce }) {
+    const { server_nonce: serverNonce, handle } = response
+    const handles = { txHandle: handle }
+    return new Transaction({ handles, clientNonce, serverNonce })
+  }
+
+  /**
+   * Redirects the main browser window to the interaction url.
+   *
+   * @param interactionUrl {string}
+   * @param transaction {Transaction}
+   */
+  interactRedirectWindow ({ interactionUrl }) {}
+
+  interactPopup ({ interactionUrl }) {
+    throw new Error('Not implemented.')
+  }
+
+  parseFromUrl (callbackUrl) {}
+
+  continueWith ({ interactHandle }) {}
+}
+
+export class TxRequest {
+  constructor ({ clientDisplay, resources, interact, key, user, capabilities }) {
+    this.display = clientDisplay
+    this.resources = resources
+    this.interact = interact
+    this.key = key
+    this.user = user
+    this.capabilities = capabilities
+  }
+
+  get clientNonce () {
+    return this.interact.redirect && this.interact.callback.nonce
+  }
+}
