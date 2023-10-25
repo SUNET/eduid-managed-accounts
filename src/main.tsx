4760e8f6ed1386ec9187f0f4988c7e9c8c89089d
Eunju Huss <eunju@sunet.se>
2023-10-25 13:54:39 +0200

    Refactoring initApp

--- src/main.tsx
+++ src/main.tsx
@@ -1,54 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ReduxIntlProvider } from './common/ReduxIntl.tsx';
-import ManagedAccountApp from './components/ManagedAccountsApp.tsx';
-import './index.css';
-import { managedAccountsStore } from './init-app.ts';
-import { jwsRequest, url } from './ts_common.ts';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ReduxIntlProvider } from "./common/ReduxIntl.tsx";
+import ManagedAccountApp from "./components/ManagedAccountsApp.tsx";
+import "./index.css";
+import { managedAccountsStore } from "./init-app.ts";
+import { jwsRequest, scimHeaders, scimRequest, scimUrl, url } from "./ts_common.ts";
 
-function setTokenFromLocalStorage(token:any) {
-  localStorage.setItem('JWSToken', token);
+function setTokenFromLocalStorage(token: any) {
+  localStorage.setItem("JWSToken", token);
 }
 
 function getTokenFromLocalStorage() {
-  return localStorage.getItem('JWSToken');
+  return localStorage.getItem("JWSToken");
 }
 
-async function getToken() {
-  console.log("App loaded..")
+async function initApp() {
+  console.log("App loaded..");
   let token = getTokenFromLocalStorage();
   if (!token) {
     try {
-      let response = await fetch(url, jwsRequest);
-      if (response.status === 401) {
-        const refreshedToken = await fetch(url, jwsRequest);
-  
-        if (refreshedToken) {
-          setTokenFromLocalStorage(refreshedToken); 
-        } else {
-          return null;
+      const response = await fetchTokenWithJws();
+      if (response?.access_token?.value) {
+        token = response.access_token.value;
+        setTokenFromLocalStorage(token);
+        const scimResponse = await fetchScimData(token);
+        if (scimResponse.status === 401) {
+          const refreshedToken = await fetchTokenWithJws();
+          if (refreshedToken) {
+            setTokenFromLocalStorage(refreshedToken);
+          } else {
+            return null;
+          }
         }
-      } else if (response instanceof Error) {
-        throw response;
+        return token;
       } else {
-        const result = await response.json();
-        token = result?.access_token?.value;
-        if (token) {
-          setTokenFromLocalStorage(token);
-        }
+        throw new Error("Invalid access token in the response.");
       }
     } catch (error) {
-      console.error('error:', error);
+      console.error("error:", error);
       return null;
     }
+  } else return await fetchScimData(token);
+}
+
+async function fetchTokenWithJws() {
+  const response = await fetch(url, jwsRequest);
+  if (response instanceof Error) {
+    throw response;
   }
-  return token;
-};
+  return await response.json();
+}
+
+async function fetchScimData(token: any) {
+  const headers = scimHeaders(token);
+  const scimResponse = await fetch(scimUrl, { ...scimRequest, headers });
+  if (scimResponse instanceof Error) {
+    throw scimResponse;
+  }
+  return await scimResponse.json();
+}
 
 const initDomTarget = document.getElementById("root");
 
-if (initDomTarget) { 
-  const root = ReactDOM.createRoot(initDomTarget); 
+if (initDomTarget) {
+  const root = ReactDOM.createRoot(initDomTarget);
   root.render(
     <React.StrictMode>
       <ReduxIntlProvider store={managedAccountsStore}>
@@ -56,5 +71,5 @@ if (initDomTarget) {
       </ReduxIntlProvider>
     </React.StrictMode>
   );
-  getToken(); 
+  initApp();
 }
