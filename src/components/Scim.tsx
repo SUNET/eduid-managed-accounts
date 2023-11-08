4db6d4bb4dabf88da5e491ab8fa06d68d3295b7d
Alessandro <alessandro.distaso@gmail.com>
2023-11-08 11:22:23 +0100

    first sketch of Scim user interface

--- src/components/Scim.tsx
+++ src/components/Scim.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 export default function Scim() {
   // const location = useLocation();
   // const { accessToken } = location.state;
-  const accessTokenTest = "add_here_access_token";
+  const [groups, setGroups] = useState<any>();
+  const [group, setGroup] = useState<any>();
+  const accessTokenTest =
+    "eyJhbGciOiJFUzI1NiJ9.eyJhdWQiOiJlZHVpZC5kb2NrZXIiLCJhdXRoX3NvdXJjZSI6ImNvbmZpZyIsImV4cCI6MTY5OTQ0MDM5NCwiaWF0IjoxNjk5NDM2Nzk0LCJpc3MiOiJhcGkuZWR1aWQuZG9ja2VyIiwibmJmIjoxNjk5NDM2Nzk0LCJyZXF1ZXN0ZWRfYWNjZXNzIjpbeyJzY29wZSI6ImVkdWlkLnNlIiwidHlwZSI6InNjaW0tYXBpIn1dLCJzY29wZXMiOlsiZWR1aWQuc2UiXSwic291cmNlIjoiY29uZmlnIiwic3ViIjoiZWR1aWRfbWFuYWdlZF9hY2NvdW50c18xIiwidmVyc2lvbiI6MX0.JFnVNUsc56VFNvDubNZnq6vCnPPxEH5wcxX0bCvGIVITNPPJd2t7hB1nrQpdarPwpWrcJhunN69MEZHGPvecaQ";
 
   // useEffect(() => {
   //   const fetchScimTest = async () => {
@@ -44,6 +49,7 @@ export default function Scim() {
       if (response.ok) {
         const resp_json = await response.json();
         console.log("[resp_json]", resp_json);
+        setGroups(resp_json.Resources);
       }
     } catch {}
   }
@@ -82,11 +88,12 @@ export default function Scim() {
     } catch {}
   }
 
-  async function getGroupInfo(e: any) {
-    e.preventDefault();
-    const form = e.target;
-    const input = form.querySelector('input[name="group_id"]');
-    const url = baseURL + "Groups/" + input.value;
+  async function getGroupDetails(id: string) {
+    // e.preventDefault();
+    // const form = e.target;
+    // const input = form.querySelector('input[name="group_id"]');
+    // const url = baseURL + "Groups/" + input.value;
+    const url = baseURL + "Groups/" + id;
     const config = {
       method: "GET",
       headers: {
@@ -105,18 +112,83 @@ export default function Scim() {
     } catch {}
   }
 
+  async function createUser(e: any) {
+    e.preventDefault();
+    const form = e.target;
+    const familyName = form.querySelector('input[name="family_name"]').value;
+    const givenName = form.querySelector('input[name="given_name"]').value;
+    const url = baseURL + "Users";
+    const payload = {
+      schemas: ["urn:ietf:params:scim:schemas:core:2.0:User"],
+      externalId: givenName,
+      name: {
+        familyName: familyName,
+        givenName: givenName,
+      },
+    };
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/scim+json",
+        // Authorization: `Bearer ${accessToken}`,
+        Authorization: `Bearer ${accessTokenTest}`,
+      },
+      body: JSON.stringify(payload),
+    };
+
+    try {
+      const response = await fetch(url, config);
+      if (response.ok) {
+        const resp_json = await response.json();
+        console.log("[resp_json]", resp_json);
+      }
+    } catch {}
+  }
+
   return (
     <>
       <h1>Scim Page</h1>
+      <h2>State (choosen)</h2>
+      {group !== undefined && (
+        <>
+          <p>
+            Group: {group.id} {group.displayName}
+          </p>
+          <button onClick={() => getGroupDetails(group.id)}>
+            Get Group Details
+          </button>
+        </>
+      )}
+      <br />
+      User:
+      <h2>Groups</h2>
       <button onClick={() => getGroups()}>Get Groups</button>
+      {groups?.map((group: any) => (
+        <li key={group.id}>
+          {group.displayName}
+          <button onClick={() => setGroup(group)}> choose group</button>
+        </li>
+      ))}
       <br />
       <form onSubmit={(e) => getGroupsSearch(e)}>
         <input name="filter_string" />
         <button type="submit">Get Groups Search</button>
       </form>
-      <form onSubmit={(e) => getGroupInfo(e)}>
+      {/* <form onSubmit={(e) => getGroupDetails(e)}>
         <input name="group_id" />
-        <button type="submit">Get Group Info</button>
+        <button type="submit">Get Group Details</button>
+      </form> */}
+      <h2>Users</h2>
+      <form onSubmit={(e) => createUser(e)}>
+        <label>
+          Family name
+          <input name="family_name" />
+        </label>
+        <label>
+          Given name
+          <input name="given_name" />
+        </label>
+        <button type="submit">create user</button>
       </form>
     </>
   );
