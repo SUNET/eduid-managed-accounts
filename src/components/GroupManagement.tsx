d5e0ed5117d89f68cf5c3055a92ce0548ef60ed2
Alessandro <alessandro.distaso@gmail.com>
2023-11-28 13:45:43 +0100

    fix check if undefined

--- src/components/GroupManagement.tsx
+++ src/components/GroupManagement.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useRef } from "react";
 import { GroupMember } from "typescript-clients/scim/models/GroupMember";
-import { createGroup, getGroupDetails, getGroupsSearch, putGroup } from "../apis/scimGroupsRequest";
+import {
+  createGroup,
+  getGroupDetails,
+  getGroupsSearch,
+  putGroup,
+} from "../apis/scimGroupsRequest";
 import { deleteUser, getUserDetails, postUser } from "../apis/scimUsersRequest";
 import { useAppDispatch, useAppSelector } from "../hooks";
 
@@ -11,7 +16,9 @@ export const GROUP_NAME = "Test Group 1";
 export default function GroupManagement() {
   const dispatch = useAppDispatch();
 
-  const managedAccountsDetails = useAppSelector((state) => state.groups.managedAccounts);
+  const managedAccountsDetails = useAppSelector(
+    (state) => state.groups.managedAccounts
+  );
   const membersDetails = useAppSelector((state) => state.members.members);
   const familyNameRef = useRef<HTMLInputElement | null>(null);
   const givenNameRef = useRef<HTMLInputElement | null>(null);
@@ -49,7 +56,9 @@ export default function GroupManagement() {
   useEffect(() => {
     console.log("FIRST ACTION");
     const initializeManagedAccountsGroup = async () => {
-      const result: any = await dispatch(getGroupsSearch({ searchFilter: GROUP_NAME }));
+      const result: any = await dispatch(
+        getGroupsSearch({ searchFilter: GROUP_NAME })
+      );
       if (getGroupsSearch.fulfilled.match(result)) {
         if (!result.payload.Resources?.length) {
           // create a new Group "managed-accounts" and set the Group ID in the state
@@ -57,9 +66,11 @@ export default function GroupManagement() {
         } else if (result.payload.Resources?.length === 1) {
           // normal case
           //setManagedAccountsGroup(result.payload.Resources[0]);
-          const response = await dispatch(getGroupDetails({ id: result.payload.Resources[0].id }));
+          const response = await dispatch(
+            getGroupDetails({ id: result.payload.Resources[0].id })
+          );
           if (getGroupDetails.fulfilled.match(response)) {
-            response.payload.members.map((member: any) => {
+            response.payload.members?.map((member: any) => {
               dispatch(getUserDetails({ id: member.value }));
             });
           }
@@ -101,8 +112,12 @@ export default function GroupManagement() {
 
   const saveUser = async (e: any) => {
     e.preventDefault();
-    const givenName = document.querySelector('[name="given_name"]') as HTMLInputElement;
-    const familyName = document.querySelector('[name="family_name"]') as HTMLInputElement;
+    const givenName = document.querySelector(
+      '[name="given_name"]'
+    ) as HTMLInputElement;
+    const familyName = document.querySelector(
+      '[name="family_name"]'
+    ) as HTMLInputElement;
     //POST USER
     if (givenName.value && familyName.value) {
       const createdUserResponse = await dispatch(
@@ -122,7 +137,10 @@ export default function GroupManagement() {
         const newGroupMember: GroupMember = {
           $ref: createdUserResponse.payload.meta?.location,
           value: createdUserResponse.payload.id,
-          display: createdUserResponse.payload.name.familyName + " " + createdUserResponse.payload.name.givenName,
+          display:
+            createdUserResponse.payload.name?.familyName +
+            " " +
+            createdUserResponse.payload.name?.givenName,
         };
 
         const newMembersList = managedAccountsDetails.members?.slice(); // copy array
@@ -148,7 +166,9 @@ export default function GroupManagement() {
 
   const removeUser = async (id: any) => {
     // 1. Remove User from Group
-    const filteredUser = managedAccountsDetails?.members?.filter((user: any) => user.value !== id);
+    const filteredUser = managedAccountsDetails?.members?.filter(
+      (user: any) => user.value !== id
+    );
     const putFilteredUserResult = await dispatch(
       putGroup({
         result: {
@@ -159,7 +179,9 @@ export default function GroupManagement() {
     );
     // 2. Delete User
     if (putGroup.fulfilled.match(putFilteredUserResult)) {
-      const memberToBeRemoved = membersDetails?.filter((user: any) => user.id === id)[0];
+      const memberToBeRemoved = membersDetails?.filter(
+        (user: any) => user.id === id
+      )[0];
       const user = {
         id: id,
         version: memberToBeRemoved.meta.version,
@@ -181,8 +203,10 @@ export default function GroupManagement() {
         <h1>Welcome to Managing Accounts using eduID</h1>
         <div className="lead">
           <p>
-            In the form below you can manage your group by adding students as members, to create the unique identifier -
-            EPPN - and the password that they will need to be able to perform the Digital National Exam. <br />
+            In the form below you can manage your group by adding students as
+            members, to create the unique identifier - EPPN - and the password
+            that they will need to be able to perform the Digital National Exam.{" "}
+            <br />
             You can also view the existing group and remove members.
           </p>
         </div>
@@ -190,17 +214,28 @@ export default function GroupManagement() {
       <section>
         <h2>Add member to group</h2>
         <ol className="listed-steps">
-          <li>Add the given name and surname to manage each member, complete one at a time.</li>
-          <li>When you click "ADD" the member will be added to the group as shown in the table below.</li>
           <li>
-            <strong>Note the corresponding EPPN and password which appears in the members table</strong>, transfer it to
-            whatever external system of your choice, as you will not be able to retrieve it afterwards.
+            Add the given name and surname to manage each member, complete one
+            at a time.
+          </li>
+          <li>
+            When you click "ADD" the member will be added to the group as shown
+            in the table below.
+          </li>
+          <li>
+            <strong>
+              Note the corresponding EPPN and password which appears in the
+              members table
+            </strong>
+            , transfer it to whatever external system of your choice, as you
+            will not be able to retrieve it afterwards.
           </li>
         </ol>
         <p>
           <em>
-            Write the name so that you can distinguish the identity of the person even if there are several students
-            with identical names e.g. by adding an initial.
+            Write the name so that you can distinguish the identity of the
+            person even if there are several students with identical names e.g.
+            by adding an initial.
           </em>
         </p>
 
@@ -222,9 +257,11 @@ export default function GroupManagement() {
             <React.Fragment>
               <h2>Manage members in group</h2>
               <p>
-                The table shows members of this group. It is not possible to edit the already added member, nor retrieve
-                a password once the session in which the member was created is ended, but by clicking "REMOVE" you can
-                remove the member and if needed create it again -<strong> with a new EPPN and password</strong>.
+                The table shows members of this group. It is not possible to
+                edit the already added member, nor retrieve a password once the
+                session in which the member was created is ended, but by
+                clicking "REMOVE" you can remove the member and if needed create
+                it again -<strong> with a new EPPN and password</strong>.
               </p>
               <table className="group-management">
                 <thead>
@@ -246,7 +283,10 @@ export default function GroupManagement() {
                       <td> {member.externalId}</td>
                       <td> </td>
                       <td>
-                        <button className="btn btn-link btn-sm" onClick={() => removeUser(member.id)}>
+                        <button
+                          className="btn btn-link btn-sm"
+                          onClick={() => removeUser(member.id)}
+                        >
                           remove
                         </button>
                       </td>
