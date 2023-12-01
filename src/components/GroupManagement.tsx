50ecd0d998182c975336bfd097c1bef184932d1e
cartja <carina@sunet.se>
2023-12-01 16:36:24 +0100

    add member validation styling

--- src/components/GroupManagement.tsx
+++ src/components/GroupManagement.tsx
@@ -1,12 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Field, Form } from "react-final-form";
 import { GroupMember } from "typescript-clients/scim/models/GroupMember";
-import {
-  createGroup,
-  getGroupDetails,
-  getGroupsSearch,
-  putGroup,
-} from "../apis/scimGroupsRequest";
+import { createGroup, getGroupDetails, getGroupsSearch, putGroup } from "../apis/scimGroupsRequest";
 import { getUserDetails, postUser } from "../apis/scimUsersRequest";
 import { onlyLetters } from "../common/regexPattern";
 import { useAppDispatch, useAppSelector } from "../hooks";
@@ -21,9 +16,7 @@ export const GROUP_NAME = "Test Group 1";
 export default function GroupManagement() {
   const dispatch = useAppDispatch();
 
-  const managedAccountsDetails = useAppSelector(
-    (state) => state.groups.managedAccounts
-  );
+  const managedAccountsDetails = useAppSelector((state) => state.groups.managedAccounts);
   const membersDetails = useAppSelector((state) => state.members.members);
   const familyNameRef = useRef<HTMLInputElement | null>(null);
   const givenNameRef = useRef<HTMLInputElement | null>(null);
@@ -41,16 +34,12 @@ export default function GroupManagement() {
       try {
         dispatch(getUsersSlice.actions.initialize());
         dispatch(getGroupsSlice.actions.initialize());
-        const result: any = await dispatch(
-          getGroupsSearch({ searchFilter: GROUP_NAME })
-        );
+        const result: any = await dispatch(getGroupsSearch({ searchFilter: GROUP_NAME }));
         if (getGroupsSearch.fulfilled.match(result)) {
           if (!result.payload.Resources?.length) {
             dispatch(createGroup({ displayName: GROUP_NAME }));
           } else if (result.payload.Resources?.length === 1) {
-            const response = await dispatch(
-              getGroupDetails({ id: result.payload.Resources[0].id })
-            );
+            const response = await dispatch(getGroupDetails({ id: result.payload.Resources[0].id }));
             if (getGroupDetails.fulfilled.match(response)) {
               response.payload.members?.map((member: any) => {
                 dispatch(getUserDetails({ id: member.value }));
@@ -82,10 +71,7 @@ export default function GroupManagement() {
           const newGroupMember: GroupMember = {
             $ref: createdUserResponse.payload.meta?.location,
             value: createdUserResponse.payload.id,
-            display:
-              createdUserResponse.payload.name?.familyName +
-              " " +
-              createdUserResponse.payload.name?.givenName,
+            display: createdUserResponse.payload.name?.familyName + " " + createdUserResponse.payload.name?.givenName,
           };
 
           const newMembersList = managedAccountsDetails.members?.slice(); // copy array
@@ -111,7 +97,7 @@ export default function GroupManagement() {
     if (values !== undefined) {
       ["given_name", "surname"].forEach((inputName) => {
         if (!values[inputName] || !onlyLetters.test(values[inputName])) {
-          errors[inputName] = "required only letters";
+          errors[inputName] = "Only letters permitted";
         }
       });
     }
@@ -131,10 +117,8 @@ export default function GroupManagement() {
         <h1>Welcome to Managing Accounts using eduID</h1>
         <div className="lead">
           <p>
-            In the form below you can manage your group by adding students as
-            members, to create the unique identifier - EPPN - and the password
-            that they will need to be able to perform the Digital National Exam.{" "}
-            <br />
+            In the form below you can manage your group by adding students as members, to create the unique identifier -
+            EPPN - and the password that they will need to be able to perform the Digital National Exam. <br />
             You can also view the existing group and remove members.
           </p>
         </div>
@@ -142,42 +126,24 @@ export default function GroupManagement() {
       <section>
         <h2>Add member to group</h2>
         <ol className="listed-steps">
+          <li>Add the given name and surname to manage each member, complete one at a time.</li>
+          <li>When you click "ADD" the member will be added to the group as shown in the table below.</li>
           <li>
-            Add the given name and surname to manage each member, complete one
-            at a time.
-          </li>
-          <li>
-            When you click "ADD" the member will be added to the group as shown
-            in the table below.
-          </li>
-          <li>
-            <strong>
-              Note the corresponding EPPN and password which appears in the
-              members table
-            </strong>
-            , transfer it to whatever external system of your choice, as you
-            will not be able to retrieve it afterwards.
+            <strong>Note the corresponding EPPN and password which appears in the members table</strong>, transfer it to
+            whatever external system of your choice, as you will not be able to retrieve it afterwards.
           </li>
         </ol>
         <p>
           <em>
-            Write the name so that you can distinguish the identity of the
-            person even if there are several students with identical names e.g.
-            by adding an initial.
+            Write the name so that you can distinguish the identity of the person even if there are several students
+            with identical names e.g. by adding an initial.
           </em>
         </p>
 
         <Form
           validate={validatePersonalData}
           onSubmit={(e) => addUser(e)}
-          render={({
-            handleSubmit,
-            form,
-            submitting,
-            pristine,
-            values,
-            invalid,
-          }) => (
+          render={({ handleSubmit, form, submitting, pristine, values, invalid }) => (
             <form
               onSubmit={async (event) => {
                 await handleSubmit(event);
@@ -187,31 +153,27 @@ export default function GroupManagement() {
               <div className="flex-between">
                 <Field name="given_name">
                   {({ input, meta }) => (
-                    <div>
+                    <fieldset>
                       <label>Given name*</label>
                       <input type="text" {...input} placeholder="given name" />
-                      {meta.touched && meta.error && <span>{meta.error}</span>}
-                    </div>
+                      {meta.touched && meta.error && <span className="input-validate-error">{meta.error}</span>}
+                    </fieldset>
                   )}
                 </Field>
 
                 <Field name="surname">
                   {({ input, meta }) => (
-                    <div>
+                    <fieldset>
                       <label>Surname*</label>
                       <input type="text" {...input} placeholder="surname" />
-                      {meta.touched && meta.error && <span>{meta.error}</span>}
-                    </div>
+                      {meta.touched && meta.error && <span className="input-validate-error">{meta.error}</span>}
+                    </fieldset>
                   )}
                 </Field>
-                <div className="buttons">
-                  <button
-                    disabled={submitting || invalid}
-                    className="btn-primary"
-                  >
-                    Add
-                  </button>
-                </div>
+
+                <button disabled={submitting || invalid} className="btn-primary">
+                  Add
+                </button>
               </div>
             </form>
           )}
