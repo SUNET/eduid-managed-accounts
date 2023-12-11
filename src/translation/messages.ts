cac9d1cecf2299a344ffbe9005eb6f6df3b50480
Eunju Huss <eunju@sunet.se>
2023-12-11 12:07:20 +0100

    Install formatcli and add scipt extract messgaes

---
+++ src/translation/messages.ts
@@ -0,0 +1,31 @@
+/*This has to be a .js file because it is imported by webpack before transpiling
+ * ... and it has to be CommonJS until we add "type": "module" to package.json.
+ */
+
+import en from "./languages/en.json";
+import sv from "./languages/sv.json";
+
+/*
+ * Transifex outputs
+ *
+ *  "login.general_failure": {
+ *   "string": "An error occurred. Please try again later."
+ *  },
+ *
+ * where react-intl wants
+ *
+ *  "login.general_failure": "An error occurred. Please try again later."
+ *
+ */
+const format_for_react_intl = (data: any) => {
+  let result: any = {};
+  Object.keys(data).forEach((k) => {
+    result[k] = data[k].string;
+  });
+  return result;
+};
+
+// the proper TypeScript type for messages would be { [key: string]: { [key: string]: string } }
+const messages = { en: format_for_react_intl(en), sv: format_for_react_intl(sv) };
+
+export { messages };
