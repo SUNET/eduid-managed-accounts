d439e339282d736757b4f43e33c33b6e6b44d5fb
Eunju Huss <eunju@sunet.se>
2024-01-10 15:23:13 +0100

    Declare managed_accounts_config

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,15 @@ import { ReduxIntlProvider } from "./common/ReduxIntl.tsx";
 import { ManagedAccountApp } from "./components/ManagedAccountsApp.tsx";
 import { managedAccountsStore } from "./init-app.ts";
 import { setupLanguage } from "./translation/index.tsx";
+declare const managed_accounts_config: object | undefined;
 
 setupLanguage(managedAccountsStore.dispatch);
 
 const initDomTarget = document.getElementById("root");
+
+if (managed_accounts_config === undefined) {
+  console.log("managed_accounts_config", managed_accounts_config);
+}
 
 if (initDomTarget) {
   const root = ReactDOM.createRoot(initDomTarget);
