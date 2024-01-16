1fdfc28b567c718146c1abf4bef3981b2d675be8
cartja <carina@sunet.se>
2024-01-16 17:14:32 +0100

    value in modal

--- src/components/MembersList.tsx
+++ src/components/MembersList.tsx
@@ -507,8 +507,9 @@ export default function MembersList({
         }
         mainText={
           <FormattedMessage
-            defaultMessage="Are you sure you want to delete ${isMemberSelected.length} accounts? If so, please press the OK button below."
+            defaultMessage="Are you sure you want to delete {selectedMemberLength} accounts? If so, please press the OK button below."
             id="manageGroup-removeMembersDialogParagraph"
+            values={{ selectedMemberLength: isMemberSelected.length }}
           />
         }
         showModal={showModal}
