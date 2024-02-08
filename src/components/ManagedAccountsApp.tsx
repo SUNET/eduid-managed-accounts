80ecfd0d3db550abb6d763503fdfa1e427184484
Eunju Huss <eunju@sunet.se>
2024-02-08 14:45:23 +0100

    Add page not found page

--- src/components/ManagedAccountsApp.tsx
+++ src/components/ManagedAccountsApp.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "../App.css";
 import Callback from "./Callback";
 import Footer from "./Footer";
 import GroupManagement from "./GroupManagement";
 import { Header } from "./Header";
 import { Notifications } from "./Notifications";
+import { PageNotFound } from "./PageNotFound";
 import { REDIRECT_PATH, StartSession } from "./StartSession";
 
 export function ManagedAccountApp(): JSX.Element {
@@ -19,7 +20,7 @@ export function ManagedAccountApp(): JSX.Element {
             <Route path="/" element={<StartSession />} />
             <Route path={REDIRECT_PATH} element={<Callback />} />
             <Route path="/manage" element={<GroupManagement />} />
-            <Route path="*" element={<Navigate to="/" replace={true} />} />
+            <Route path="*" element={<PageNotFound />} />
           </Routes>
         </section>
       </main>
