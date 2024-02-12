48031499ef9cdf6135bdc8ab4d0d661722e4cb17
Eunju Huss <eunju@sunet.se>
2024-02-12 10:05:10 +0100

    currentdate -> currentDate

--- src/common/time.ts
+++ src/common/time.ts
@@ -1,12 +1,12 @@
 export default function currentDateTimeToString(): string {
-  const currentdate = new Date();
+  const currentDate = new Date();
   const currentDateTime =
-    currentdate.getFullYear() +
-    ("0" + (currentdate.getMonth() + 1)).slice(-2) +
-    ("0" + currentdate.getDate()).slice(-2) +
+    currentDate.getFullYear() +
+    ("0" + (currentDate.getMonth() + 1)).slice(-2) +
+    ("0" + currentDate.getDate()).slice(-2) +
     "-" +
-    ("0" + currentdate.getHours()).slice(-2) +
-    ("0" + currentdate.getMinutes()).slice(-2) +
-    ("0" + currentdate.getSeconds()).slice(-2);
+    ("0" + currentDate.getHours()).slice(-2) +
+    ("0" + currentDate.getMinutes()).slice(-2) +
+    ("0" + currentDate.getSeconds()).slice(-2);
   return currentDateTime;
 }
